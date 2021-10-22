8bf9d45868d405bf86a2bcdc5f7493c3f525a038
GEONHO HAN <43363413+borish3198@users.noreply.github.com>
2021-10-22 21:54:49 +0900

    Update trading_pr.py
    기존 프로그램 로직 : 프로그램 시작 - 코인 진입 가격 계산 및 보유코인 매도 - 가격 추적 - 프로그램 종료
    수정된 프로그램 로직 : 프로그램 시작 - 코인 진입 가격 계산 - 가격추적 - 종료 시간 설정(종료 시간에 코인 매도) - 프로그램 종료

--- trading_pr.py
+++ trading_pr.py
@@ -9,10 +9,11 @@
 import pybithumb
 import datetime
 import time
+import cv2
 
 def main():
     # API key 값 받아오기
-    with open("key값이 있는 txt파일") as f:
+    with open("bithumb.txt") as f:
         lines = f.readlines()
         key = lines[0].strip()
         secret = lines[1].strip()
@@ -31,7 +32,7 @@ def main():
         df = pybithumb.get_ohlcv(ticker)
         yesterday = df.iloc[-2]
 
-        k = 0.5                                    # 매수 포인트를 가늠하는 중요한 지표로, 높을수록 더욱 확실할때에 진입한다.
+        k = 0.5
         today_open = yesterday["close"]
         yesterday_high = yesterday["high"]
         yesterday_low = yesterday["low"]
@@ -53,24 +54,23 @@ def main():
         bithumb.sell_market_order(ticker, unit)
 
 
-
     #매일 자정 거래를 위한 시간값
     now = datetime.datetime.now()
     print("프로그램 시작 시간 :", now)
 
-    test = now + datetime.timedelta(seconds=10)
-    # print(test)
+    start = now + datetime.timedelta(seconds=10)
+    end = datetime.datetime(now.year, now.month, now.day, 23, 55, 00)
 
     count=0
     target_price = {}
     ma5 = {}
     buy_flag = {}
-    coins = ["거래 희망하는 코인의 코드 값을 리스트 형태로 입력"]       #예시) coins = ['BTC', 'ETH', 'XRP', 'ADA',  'LTC', 'BCH']
+    coins = ['BTC', 'ETH', 'XRP', 'ADA',  'LTC', 'BCH']
 
     while True:
         try:
             now = datetime.datetime.now()
-            if test < now < test + datetime.timedelta(seconds=2): 
+            if start < now < start + datetime.timedelta(seconds=2):  #테스트용 거래 시간
                 print(now, '거래를 시작합니다.')
 
                 #모두 다 초기화 
@@ -79,7 +79,7 @@ def main():
                 buy_flag = {}
                 conut = 0
 
-                # 보유 코인 모두 판매
+                # 코인 별 진입가격 계산
                 for tick in coins:
                     target_price[tick] = get_target_price(tick)            # 코인 별 진입 가격 계산
                     
@@ -87,16 +87,18 @@ def main():
                     buy_flag[tick] = 0                                     # buy_flag에 초기값 0
                                     
                     balance = bithumb.get_balance(tick)
-
-                    if balance[0]!=0:
-                        sell_crypto_currency(tick)
-                        print("매도 완료 코인 :", tick)
             
                 print(target_price)
                 print(ma5)
                 print("코인 매도 완료, 매수 실행")
 
-            if count>=6:                              # 거래 대상에 속한 코인의 개수에 따라 조건값 수정 필요
+            # 이동평균 조건 및 진입 가격 먼저 도달하는 상위 20개만 매수(4.13 기준 151개)
+            # 5.4 기준 ['BTC', 'ETH', 'XRP', 'ADA', 'LTC', 'BCH'] 해당 코인 6개만 거래
+            # 탐색순서에 영향을 많이 받음 / get_tickers() 함수에서 받아오는 순서대로 탐색
+            # 보완할 필요 있음, 변동성 돌파 전략 및 이동평균 자격 충족하는 화폐들 리스트 따로 만든 후
+            # 그중에 우선순위 판단하여 거래할 수 있도록 보완 필
+
+            if count>=6:
                 print("금일 거래를 종료합니다.")
                 break
 
@@ -108,17 +110,28 @@ def main():
                 current_price = pybithumb.get_current_price(tick)
                 if (current_price>target_price[tick]) and (current_price > ma5[tick]) and (buy_flag[tick] == 0):
                     k=6-count
-                    # print("지표2")
                     buy_crypto_currency(tick, k)
                     result = bithumb.get_balance(tick)[0]
                     buy_flag[tick]=1
-                    # print("지표3")
                     count+=1
                     print(count, "번째 코인 매수 완료\n거래코인 :", tick, "\n거래수량 :", result)
                     print("이동평균 :", ma5[tick], "현재가 :", current_price, "진입가 :", target_price[tick])
                     print("--------------------------------------------------------------------------------")
-                        
+            
+            now = datetime.datetime.now()
+            if end < now < end + datetime.timedelta(seconds=2):  #매도 시점 매일 자정에 근접하여 매도
+                # 보유 코인 모두 판매
+                for tick in coins:
+                    balance = bithumb.get_balance(tick)
+                    if balance[0]!=0:
+                        sell_crypto_currency(tick)
+                        print("매도 완료 코인 :", tick)                
+                print(now, '거래를 종료합니다.')
+                break
+                
         except:
             print("에러 발생")
-        
+
+        if cv2.waitKey(1) == ord('q'):
+            break
         time.sleep(1)
